8cfa4972f714ef522b48f7c68d41051aa6d60c98
JIMMY\jimmyR <jimmy.ramos@minesdedouai.fr>
2017-04-15 17:46:21 +0200

    Ajout de la template "gamePlayed" Il s'agit de la template montré aux utilisateur ayant déjà joué leurs cartes durant ce tour de jeu. Dans cette template, ils ne peuvent pas jouer, donc le bouton ou le preocessus servant à joueur une carte (je te laisse gérer Ayoub) ne doit pas être présent. On y voit just les infos
    La seul diffrence avec la template game est donc que le bouton jouer n'est pas présent, le reste y est.

---
+++ templates/gamePlayedTemplate.php
@@ -0,0 +1,79 @@
+<div id="cartePlateau">
+	<FONT color="white"><b>
+		<?php 
+		echo "plateau :<br>";
+			if($cardPil1 != NULL){
+				foreach ($cardPil1 as $p1) {
+					echo "carte: ".$p1->NUMERO." | point: ".$p1->POINT." ---------- ";
+				}
+			}	
+			echo "<br>";
+			if($cardPil2 != NULL){
+				foreach ($cardPil2 as $p2) {
+					echo "carte: ".$p2->NUMERO." | point: ".$p2->POINT." ---------- ";
+				}
+			}	
+			echo "<br>";
+			if($cardPil3 != NULL){
+				foreach ($cardPil3 as $p3) {
+					echo "carte: ".$p3->NUMERO." | point: ".$p3->POINT." ---------- ";
+				}
+			}	
+			echo "<br>";
+			if($cardPil4 != NULL){
+				foreach ($cardPil4 as $p4) {
+					echo "carte: ".$p4->NUMERO." | point: ".$p4->POINT." ---------- ";
+				}
+			}	
+			echo "<br>";
+		?>
+		<br> Ayoub : replacer ca par des images (faire/trouver les images, les stoker, mettre leurs path dans la base de données (je m'en occupe une fois qu'on a les images) et afficher tout ca). Pour l'instant, prépare des images vierge pour tester <br><br>
+	</b></FONT>
+</div>
+
+<div id="main">
+	<FONT color="red"><b>
+		<?php 
+			echo "Ma main :<br>";
+			if($handCard != NULL){
+				foreach ($handCard as $hc) {
+					echo "carte: ".$hc->NUMERO." | point: ".$hc->POINT." ---------- ";
+				}
+			}
+			echo "<br>";
+			?>
+	</b></FONT>
+</div>
+
+<div id="cardPut">
+	<FONT color="blue"><b>
+	<br><br>
+		<?php 
+			echo "Nombre de carte posé par tout les joueurs pendant cette manche(soit compris) / nombre de joueur :<br>";
+			if($cardPut != NULL){
+				echo "carte posée: " . count($cardPut) . " / ". count($participant);
+			}
+			else{
+				echo "carte posée: 0 / ". count($participant);
+			}
+			echo "<br>";
+			?>
+	</b></FONT>
+</div>
+
+<div id="participant">
+	<FONT color="green"><b>
+	<br><br>
+		<?php 
+			echo "joueur et score<br>";
+			foreach ($participant as $p) {
+				echo "Joueur: ".$p->PSEUDO." | photoProfil: ".$p->PHOTOPROFIL." | score: ". $p->SCORE ." ---------- <br>";
+			}	
+		?>
+	</b></FONT>
+</div>
+<FONT color="white"><b><br>
+NOus sommes dans la templat gamePlayedTemplate!!!<br>
+La seul différence avec le gameTemplate est que le joueur qui est redirigé ici ne peut pas jouer une carte (il a déjà joué sa carte, donc le bouton pour joueur ne sera pas présent ici)
+Cette page est lancé seulement pour les joueurs qui ont déjà joué dans leurs manches. Et l'autre page sera lancé quand tout les joueurs auront joué.
+</b></FONT>
