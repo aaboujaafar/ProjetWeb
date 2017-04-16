d2f8b01f45ff45dcb97451bd9c9da785c1e7810f
ayoub-ab <ayoub.aboujaafar@minesdedouai.fr>
2017-04-16 22:35:26 +0200

    Front End : ajout nbr de participant

--- templates/joinGameTemplate.php
+++ templates/joinGameTemplate.php
@@ -5,30 +5,36 @@
 		echo '<ul class="list-group col-md-4">
 			<li class="list-group-item title titrePB" >Les parties publiques</li>';
 		if($publicGame != NULL){
+			$i =0;
 			foreach ($publicGame as $pg){
 				echo'<li class="list-group-item top15gamer">
-				<span id="pseudoTop">'.$pg->NOMPARTIE.'</span>
+				<span id="pseudoTop">'.$pg->NOMPARTIE.'</span><span class="badge monBadgeRP" data-toggle="tooltip" title="Nombre de participants" data-placement="bottom">'.$nbrPublicGame[$i].'</span>
 				 </li>';
+				 $i+=1;
 			}
 		}
 		echo '</ul>';
 		echo '<ul class="list-group col-md-4">
 			<li class="list-group-item title titrePB" >Les parties en tant que participant</li>';
 		if($userGame != NULL){
+			$i =0;
 			foreach ($userGame as $ug){
 				echo'<li class="list-group-item top15gamer">
-				<span id="pseudoTop">'.$ug->NOMPARTIE.'</span>
+				<span id="pseudoTop">'.$ug->NOMPARTIE.'</span><span class="badge monBadgeRP" data-toggle="tooltip" title="Nombre de participants">'.$nbrUserGame[$i].'</span>
 				 </li>';
+				 $i+=1;
 			}
 		}
 		echo '</ul>';
 		echo '<ul class="list-group col-md-4">
 			<li class="list-group-item title titrePB" >Les parties en tant que créateur</li>';
 		if($ownerGame != NULL){
+			$i =0;
 			foreach ($ownerGame as $og){
 				echo'<li class="list-group-item top15gamer">
-				<span id="pseudoTop">'.$og->NOMPARTIE.'</span>
+				<span id="pseudoTop">'.$og->NOMPARTIE.'</span><span class="badge monBadgeRP" data-toggle="tooltip" title="Nombre de participants">'.$nbrOwnerGame[$i].'</span>
 				 </li>';
+				 $i+=1;
 			}
 		}
 		echo '</ul><br>';
