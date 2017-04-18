e5614ddcbae8d6b2c16f207c906a387152a08367
ayoub-ab <ayoub.aboujaafar@minesdedouai.fr>
2017-04-18 12:28:51 +0200

    Ajout des cartes

--- templates/gameTemplate.php
+++ templates/gameTemplate.php
@@ -1,30 +1,30 @@
 <div id="cartePlateau">
 	<FONT color="white"><b>
-		<?php 
+		<?php
 		echo "plateau :<br>";
 			if($cardPil1 != NULL){
 				foreach ($cardPil1 as $p1) {
 					echo "carte: ".$p1->NUMERO." | point: ".$p1->POINT." ---------- ";
 				}
-			}	
+			}
 			echo "<br>";
 			if($cardPil2 != NULL){
 				foreach ($cardPil2 as $p2) {
-					echo "carte: ".$p2->NUMERO." | point: ".$p2->POINT." ---------- ";
+					echo 'carte:<img src="img/cartes/'.$p2->NUMERO.'.png" /> | point: ".$p2->POINT." ---------- ";
 				}
-			}	
+			}
 			echo "<br>";
 			if($cardPil3 != NULL){
 				foreach ($cardPil3 as $p3) {
 					echo "carte: ".$p3->NUMERO." | point: ".$p3->POINT." ---------- ";
 				}
-			}	
+			}
 			echo "<br>";
 			if($cardPil4 != NULL){
 				foreach ($cardPil4 as $p4) {
 					echo "carte: ".$p4->NUMERO." | point: ".$p4->POINT." ---------- ";
 				}
-			}	
+			}
 			echo "<br>";
 		?>
 		<br> Ayoub : replacer ca par des images (faire/trouver les images, les stoker, mettre leurs path dans la base de données (je m'en occupe une fois qu'on a les images) et afficher tout ca). Pour l'instant, prépare des images vierge pour tester <br><br>
@@ -33,7 +33,7 @@
 
 <div id="main">
 	<FONT color="red"><b>
-		<?php 
+		<?php
 			echo "Ma main :<br>";
 			if($handCard != NULL){
 				foreach ($handCard as $hc) {
@@ -48,7 +48,7 @@
 <div id="cardPut">
 	<FONT color="blue"><b>
 	<br><br>
-		<?php 
+		<?php
 			echo "Nombre de carte posé par tout les joueurs pendant cette manche(soit compris) / nombre de joueur :<br>";
 			if($cardPut != NULL){
 				echo "carte posée: " . count($cardPut) . " / ". count($participant);
@@ -64,11 +64,11 @@
 <div id="participant">
 	<FONT color="green"><b>
 	<br><br>
-		<?php 
+		<?php
 			echo "joueur et score<br>";
 			foreach ($participant as $p) {
 				echo "Joueur: ".$p->PSEUDO." | photoProfil: ".$p->PHOTOPROFIL." | score: ". $p->SCORE ." ---------- <br>";
-			}	
+			}
 		?>
 	</b></FONT>
 </div>
